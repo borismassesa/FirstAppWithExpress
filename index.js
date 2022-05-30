91fff29b27de24f6aa2b05a88eb5bfa8fb7db2f8
borismassesa <bmmassesa@gmail.com>
2022-05-29 18:19:02 -0700

    Working with Query Strings with Express js

--- index.js
+++ index.js
@@ -6,30 +6,45 @@ const app = express();
 //   //console.dir(res)
 //   res.send("Hello, We got your request!!")
 // })
+app.listen(3000, () => {
+  console.log("Listenning on port 3000!")
+})
+
 app.get("/r/:subreddit", (req,res) => {
   const {subreddit} = req.params;
   res.send(`Browsing ${subreddit} subreddit.`)
 })
+
 app.get("/r/:subreddit/:postID", (req,res) => {
   const {subreddit,postID} = req.params;
   res.send(`Viewing Post ID: ${postID} on a browsing ${subreddit} subreddit.`)
 })
+
 app.get("/", (req,res) => {
   res.send("Welcome to the home page")
 })
+
 app.get("/cats", (req,res) => {
   res.send("Meow!!");
 })
+
 app.get("/dogs", (req,res) => {
   res.send("Wooof..")
 })
+
 app.post("/boris", (res, req) => {
   res.send("This is boris trying to use a post request")
 })
+
+app.get('/search', (req, res) => {
+  const { q } = req.query;
+  if(!q){
+    res.send(`<h1> Nothing found if nothing searched. </h1>`)
+  }
+  res.send(`<h1> Search results for: ${q} </h1>`)
+})
+
 app.get('*', (req,res) => {
   res.send("Oooopppps.... I dont know that path")
 })
 
-app.listen(3000, () => {
-  console.log("Listenning on port 3000!")
-})
